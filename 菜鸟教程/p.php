404ec835babf62a9498ee770d3474c6a1c886f64
Greenbirch2007 <291109028@qq.com>
2020-10-02 18:59:09 +0800

    10.2 chg

--- 菜鸟教程/p.php
+++ 菜鸟教程/p.php
@@ -1,5 +1,27 @@
 <?php
-namespace Mylll;
+interface iTemplate
 
-echo __NAMESPACE__;
+{
+    public function setVariable($name,$var);
+    public function getHtml($template);
+}
+
+class Template implements iTemplate
+{
+    private $vars = array();
+    public function setVariable($name, $var)
+    {
+        $this->vars[$name]=$var;   // TODO: Implement setVariable() method.
+    }
+    public function getHtml($template)
+    {
+        foreach ($this->vars as $name => $value){
+            $template =str_replace('{.$name.}',$value,$template)
+        }
+        return $template;
+           // TODO: Implement getHtml() method.
+    }
+}
+
+
 ?>
