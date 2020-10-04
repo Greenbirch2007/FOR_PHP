56dac004ba6ef36740ebca06c2045eeddf61c82c
Greenbirch2007 <291109028@qq.com>
2020-10-04 13:47:48 +0800

    10.4 chg

--- 菜鸟教程/p.php
+++ 菜鸟教程/p.php
@@ -1,27 +1,35 @@
 <?php
-interface iTemplate
 
-{
-    public function setVariable($name,$var);
-    public function getHtml($template);
-}
+echo strpos("You love php, I love php too!","php");
+//include  include_once
+//array_keys()
 
-class Template implements iTemplate
-{
-    private $vars = array();
-    public function setVariable($name, $var)
-    {
-        $this->vars[$name]=$var;   // TODO: Implement setVariable() method.
-    }
-    public function getHtml($template)
-    {
-        foreach ($this->vars as $name => $value){
-            $template =str_replace('{.$name.}',$value,$template)
-        }
-        return $template;
-           // TODO: Implement getHtml() method.
-    }
-}
+//echo print
+//upload_max_filesize() 2mb
 
+//set_time_limit() 30s
+// $_SERVER['REMOETE_ADDR'] 获取客户端IP
+//
+//$_SESSION//用于存储当前会话的变量
+
+//cookie存储用户本地磁盘上，里面保存着浏览器的
+//网页的一些信息
+// php配置文件php.ini是影响php功能的最终也是最直接的方法
+//每次初始化php时都会读取php.ini文件
+//NULL是一种特殊类型，只有一个值NULL//
+//__FILE__文件的完整路径和文件名。
+//$this 引用类本身中的属性或方法
+//对空格不敏感。
+//PHP执行系统功能，即从系统上的文件创建，打开，读取，写入和关闭它们
+//PHP可以处理表单，即从文件中收集数据，
+//将数据保存到文件中，通过电子邮件发送数据，将数据
+//返回给用户
+//可以通过PHP在数据库中添加，删除fetch_row()
+
+//构造函数可以接受参数
+//构造函数可以调用类型方法或其他函数
+//类构造函数可以调用其他类的构造函数
+
+preg_match()
 
 ?>
